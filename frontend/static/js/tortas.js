3718137d81a31d8a59f10fbf93aa3b768713fc6c
trinidadperea <trinidadperea10@gmail.com>
2026-08-31 16:16:19 -0300

    Arreglo error wpp

--- frontend/static/js/tortas.js
+++ frontend/static/js/tortas.js
@@ -1,3 +1,32 @@
+
+const whatsappButtons = document.querySelectorAll(".whatsapp-button");
+
+whatsappButtons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const torta = button.dataset.torta;
+
+        const message = `Hola Elva Bakery!
+
+Quiero consultar por:
+
+- ${torta}
+
+¡Gracias! 💕`;
+
+        const phone = "5492616175138";
+
+        const url =
+            `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+
+        window.open(url, "_blank");
+
+    });
+
+});
+
+
 const tortasCarousel = [
 
     {
@@ -154,30 +183,3 @@ crearIndicadoresCarousel();
 
 
 
-
-const whatsappButtons = document.querySelectorAll(".whatsapp-button");
-
-whatsappButtons.forEach(button => {
-
-    button.addEventListener("click", () => {
-
-        const torta = button.dataset.torta;
-
-        const message = `Hola Elva Bakery!
-
-Quiero consultar por:
-
-- ${torta}
-
-¡Gracias! 💕`;
-
-        const phone = "5492616175138";
-
-        const url =
-            `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
-
-        window.open(url, "_blank");
-
-    });
-
-});
